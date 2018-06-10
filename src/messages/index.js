e1e0194508719754319535bd58dab4c0c5d088e0
JGAntunes <j.goncalo.antunes@gmail.com>
2018-06-10 22:21:27 +0100

    chore: remove file extensions

--- src/messages/index.js
+++ src/messages/index.js
@@ -2,8 +2,8 @@
 
 const protons = require('protons')
 
-const rpc = protons(require('./rpc.proto.js'))
-const topic = protons(require('./topic.proto.js'))
+const rpc = protons(require('./rpc.proto'))
+const topic = protons(require('./topic.proto'))
 
 module.exports = {
   rpc,
