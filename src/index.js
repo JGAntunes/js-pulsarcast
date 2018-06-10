2d6f0cc7cd6f81d577d63f8bd15bd7ba851421de
JGAntunes <j.goncalo.antunes@gmail.com>
2018-06-10 22:22:24 +0100

    chore: working on pulsarcast class

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@
 
 const EventEmitter = require('events')
 const assert = require('assert')
+const lp = require('pull-length-prefixed')
+const pull = require('pull-stream')
 
-// const log = require('utils/logger')
+const log = require('utils/logger')
+
+const pb = require('./messages')
+const { protocol } = require('./config')
+const Peer = require('./peer')
+const rpc = require('./rpc')
 
 class Pulsarcast extends EventEmitter {
   constructor (libp2p) {
@@ -26,13 +33,65 @@ class Pulsarcast extends EventEmitter {
     this.subscriptions = new Set()
 
     this._onConnection = this._onConnection.bind(this)
-    this._dialPeer = this._dialPeer.bind(this)
+
+    // Create our handlers to receive and send RPC messages
+    this.rpc = rpc(this)
+  }
+
+  _addPeer (peerInfo, conn) {
+    const idB58Str = peerInfo.id.toB58String()
+    // Check to see if we already have the peer registered
+    let peer = this.peers.get(idB58Str)
+    if (peer) {
+      peer.attachConnection(conn)
+      return peer
+    }
+    // Create a new peer and insert it in the map
+    peer = new Peer(peerInfo, conn)
+    this.peers.set(peerInfo.id.toB58String(), peer)
+    return peer
+  }
+
+  _onConnection (protocol, conn) {
+    conn.getPeerInfo((err, peerInfo) => {
+      if (err) {
+        log.err('Failed to identify incomming conn', err)
+        return pull(pull.empty(), conn)
+      }
+
+      const idB58Str = peerInfo.id.toB58String()
+      const peer = this._addPeer(peerInfo, conn)
+
+      this._processConnection(idB58Str, conn, peer)
+    })
+  }
+
+  _processConnection (idB58Str, conn, peer) {
+    pull(
+      conn,
+      lp.decode(),
+      pull.map((data) => pb.rpc.RPC.decode(data)),
+      pull.drain(
+        (rpc) => this._onRPC(idB58Str, rpc),
+        (err) => this._onConnectionEnd(idB58Str, peer, err)
+      )
+    )
+  }
+
+  _onRPC (idB58Str, rpc) {
+    log(`Message from ${idB58Str}`)
+  }
+
+  _onConnectionEnd (idB58Str, peer, err) {
+    log(`Error from ${idB58Str}`, err)
   }
 
   start (callback) {
     if (this.started) {
       return setImmediate(() => callback(new Error('already started')))
     }
+
+    this.libp2p.handle(protocol, this._onConnection)
   }
 
   stop (callback) {
