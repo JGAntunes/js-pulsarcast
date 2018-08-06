0995e762160828961e47059781b9b531a643fcb6
JGAntunes <j.goncalo.antunes@gmail.com>
2018-08-06 23:59:54 +0100

    fix: messages schema export

--- src/messages/index.js
+++ src/messages/index.js
@@ -4,8 +4,10 @@ const protons = require('protons')
 
 const rpc = protons(require('./rpc.proto'))
 const topic = protons(require('./topic.proto'))
+const schemas = require('./schemas')
 
 module.exports = {
   rpc,
-  topic
+  topic,
+  schemas
 }
