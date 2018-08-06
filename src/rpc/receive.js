dca1deeb9b5bba33f3b018f4a0cfad066c2d6c9d
JGAntunes <j.goncalo.antunes@gmail.com>
2018-08-06 23:59:20 +0100

    chore: working on message unmarshalling

--- src/rpc/receive.js
+++ src/rpc/receive.js
@@ -1,7 +1,26 @@
 'use strict'
 
+const Joi = require('joi-browser')
+const bs58 = require('bs58')
+
 // const log = require('utils/logger')
 const ops = require('../messages').rpc.RPC.Operation
+const schemas = require('../messages').schemas
+
+function unmarshallMessage (message) {
+  const result = {}
+  result.topic = bs58.encode(message.topic)
+  result.event = {
+    publisher: bs58.encode(message.event.publisher),
+    payload: JSON.stringify(message.event.payload.toString('utf8')),
+    parent: bs58.encode(message.event.parent),
+    metadata: message.event.metadata
+  }
+  // Convert OP to a valid uppercase string
+  result.op = Object.entries(ops).find(([op, value]) => {
+    return value === message.op
+  })[0]
+}
 
 function createRPCHandlers (pulsarcastNode) {
   return {
@@ -21,7 +40,8 @@ function createRPCHandlers (pulsarcastNode) {
   }
 
   function join (idB58Str, message) {
-
+    // TODO perform join validation here
+    const topic = message.topic
   }
 
   function leave (idB58Str, message) {
@@ -29,18 +49,31 @@ function createRPCHandlers (pulsarcastNode) {
   }
 
   function genericHandler (idB58Str, message) {
-    // TODO perform generic validation here
-    switch (message.op) {
+    const result = Joi.validate(message, schemas.rpc, {
+      abortEarly: true,
+      allowUnknown: false,
+      convert: true
+    })
+
+    if (result.error) {
+      // TODO log error
+      return
+    }
+    // We use the resulting message from the validation
+    // with type coercion
+    const jsonMessage = unmarshallMessage(result.message)
+
+    switch (jsonMessage.op) {
       // case ops.PING:
-      //   return ping(idB58Str, message)
+      //   return ping(idB58Str, jsonMessage)
       case ops.UPDATE:
-        return update(idB58Str, message)
+        return update(idB58Str, jsonMessage)
       case ops.EVENT:
-        return event(idB58Str, message)
+        return event(idB58Str, jsonMessage)
       case ops.JOIN:
-        return join(idB58Str, message)
+        return join(idB58Str, jsonMessage)
       case ops.LEAVE:
-        return leave(idB58Str, message)
+        return leave(idB58Str, jsonMessage)
     }
   }
 }
