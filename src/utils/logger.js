5ee9cb63d683b956ae41aae9295b67f4871e5121
JGAntunes <j.goncalo.antunes@gmail.com>
2018-08-07 01:01:10 +0100

    chore: log methods

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,5 +4,8 @@ const debug = require('debug')
 
 const log = debug('libp2p:pulsarcast')
 log.error = log.err = debug('libp2p:pulsarcast:error')
+log.info = debug('libp2p:pulsarcast:info')
+log.debug = debug('libp2p:pulsarcast:debug')
+log.trace = debug('libp2p:pulsarcast:trace')
 
 module.exports = log
